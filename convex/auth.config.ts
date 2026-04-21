9ab40ca8406af59dbb60a9e720309866b94350b2
Catorious Black <145537060+wolfspace099@users.noreply.github.com>
2026-04-21 13:40:56 +0200

    auth fix

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from "convex/server";
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN!,
+      domain: "https://clerk.cqtdev.cc",
       applicationID: "convex",
     },
   ],
